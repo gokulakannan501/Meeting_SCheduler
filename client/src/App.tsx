be3ad7179a98ea660831a40cf90c24cd81b3f35e
Vishwajeet <netra@bitanz.com>
2026-02-17 16:46:44 +0530

    Fix unused React imports causing build failure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import Login from './Login';
